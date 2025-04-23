9ed6a724d476b3174b723027599c0c116a2a65e9
ArunMAT86 <arunmathew009@gmail.com>
2025-04-22 22:08:52 -0400

    login page started

---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  
+  username =  '';
+  password = '';
+
+goToSignup() {
+throw new Error('Method not implemented.');
+}
+
+onSubmit() {
+throw new Error('Method not implemented.');
+}
+
+
+
+
+
+}
